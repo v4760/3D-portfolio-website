c08cfc2eede9326739dc50611950357327d1503e
Vikas Kagawad <vikaskagawad@gmail.com>
2025-01-10 14:28:54 -0500

    first set of addition

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
+import { nav } from "framer-motion/client";
+
+const Navbar = () => {
+  return (
+    <nav className={`${
+        styles.paddingX
+      } w-full flex items-center py-5 fixed top-0 z-20 bg-primary
+      }`}
+    >
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+        <Link
+            to='/'
+            className='flex items-center gap-2'
+            onClick={() => {
+              setActive("");
+              window.scrollTo(0, 0);
+            }}
+          >
+            <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
+            <p className='text-white text-[18px] font-bold cursor-pointer flex '>
+              Adrian &nbsp;
+              <span className='sm:block hidden'> | JavaScript Mastery</span>
+            </p>
+          </Link>
+
+          
+
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
